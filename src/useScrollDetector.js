66454f3ea2d60ab9db17ce47f39d93553144837d
Kervin Vasquez <kervinlvh@gmail.com>
2020-05-23 15:59:28 -0700

    Initial commit

--- src/useScrollDetector.js
+++ src/useScrollDetector.js
@@ -2,9 +2,20 @@ import { useState, useEffect } from "react";
 
 export default function useScrollDetector() {
   const [position, setPosition] = useState(0);
+  const [prevPosition, setPrevPosition] = useState(0);
   const [isScrolling, setIsScrolling] = useState(false);
   const [scrollingElement, setScrollingElement] = useState(null);
-  let scrollTimeout = null;
+  const [direction, setDirection] = useState("stopped");
+
+  function calculateAndSetDirection(oldPosition, newPosition) {
+    if (newPosition > oldPosition) {
+      setDirection("down");
+    } else if (newPosition < oldPosition) {
+      setDirection("up");
+    } else {
+      setDirection("stopped");
+    }
+  }
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -13,13 +24,15 @@ export default function useScrollDetector() {
   }, []);
 
   useEffect(() => {
+    let scrollTimeout = null;
     if (scrollingElement) {
       scrollingElement.addEventListener("scroll", (e) => {
         clearTimeout(scrollTimeout);
         setIsScrolling(true);
-        setPosition(scrollingElement.pageYOffset);
+        setPrevPosition(scrollingElement.pageYOffset);
         scrollTimeout = setTimeout(function () {
           setIsScrolling(false);
+          setDirection("stopped");
         }, 66);
       });
     }
@@ -29,10 +42,19 @@ export default function useScrollDetector() {
         scrollingElement.removeEventListener("scroll", (e) => {
           setIsScrolling(false);
           setPosition(0);
+          setPrevPosition(0);
+          setDirection("stopped");
         });
       }
     };
   }, [scrollingElement]);
 
-  return [isScrolling, position];
+  useEffect(() => {
+    if (scrollingElement && position !== scrollingElement.pageYOffset) {
+      calculateAndSetDirection(position, scrollingElement.pageYOffset);
+      setPosition(scrollingElement.pageYOffset);
+    }
+  }, [isScrolling, prevPosition, position, scrollingElement]);
+
+  return [isScrolling, direction, position];
 }
